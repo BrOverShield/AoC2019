da22740dbcba6c920fbbf083f9aa850c9dbf9ec6
Alex CCTT <alexandre.lessard@cyberquebec.org>
2019-12-05 00:18:17 -0500

    created folders

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -9,39 +9,54 @@ int main()
     int NumberB = 675869;
     int Difference = NumberB - NumberA;
     std::vector<int> Range(Difference);
-    std::iota(Range.begin(), Range.end(), 1);
+    std::iota(Range.begin(), Range.end(), 172851);
     int LastDigit;
     int Digit;
+    int ThirdDigit;
     bool HasDoubleDigit = false;
+    int DoubleDigitNumber = -1;
     bool NeverDecreases = true;
     int Result = 0;
     // std::vector<int> test;
-    // test.push_back(122345);
-    // test.push_back(111123);
-    // test.push_back(135679);
-    // test.push_back(111111);
-    // test.push_back(223450);
-    // test.push_back(123789);
+    // test.push_back(112233);
+    // test.push_back(123444);
+    // test.push_back(111122);
     for (auto number : Range)
     {
         std::string sNumber = std::to_string(number);
         LastDigit = -1;
+        ThirdDigit = -1;
         HasDoubleDigit = false;
         NeverDecreases = true;
+        DoubleDigitNumber = -1;
         for (auto cDigit : sNumber)
         {
             Digit = (int)cDigit;
             if (LastDigit != -1)
             {
-                if (LastDigit == Digit)
+                if (!HasDoubleDigit)
                 {
-                    HasDoubleDigit = true;
+                    if (Digit == LastDigit && LastDigit != ThirdDigit)
+                    {
+                        HasDoubleDigit = true;
+                        DoubleDigitNumber = Digit;
+                    }
                 }
+                else
+                {
+                    if (DoubleDigitNumber == Digit && Digit == ThirdDigit)
+                    {
+                        HasDoubleDigit = false;
+                    }
+                }
+                
+                
                 if (LastDigit > Digit)
                 {
                     NeverDecreases = false;
                 }
             }
+            ThirdDigit = LastDigit;
             LastDigit = Digit;
         }
         if (NeverDecreases && HasDoubleDigit)
